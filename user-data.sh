1d0c4f78fec99d35d667241c66f2a4702528acf1
Obinna <obinnaozigbu@gmail.com>
2026-04-17 00:01:54 +0100

    Modified user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+set -e
+
 apt update -y
 apt install nginx git -y
 
 systemctl enable nginx
 systemctl start nginx
 
-cd /var/www/html
-rm -rf *
-
-git clone https://github.com/obinnaozigbu-svg/devops-asg.git .
+rm -rf /var/www/html/*
+git clone https://github.com/obinnaozigbu-svg/devops-asg.git /var/www/html
 
 systemctl restart nginx
