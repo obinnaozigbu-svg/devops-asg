b5a80f1a46483b15ff3f389b19b2365a6b36bc06
Obinna <obinnaozigbu@gmail.com>
2026-05-06 00:14:58 +0100

    Test after main.tf fix

--- terraform-ecs-project/main.tf
+++ terraform-ecs-project/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "obinna-terraform-state-bucket"
-    key            = "ecs-project/terraform.tfstate"
+    key            = "terraform-ecs-project/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
   }
